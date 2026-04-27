e5f1026906b12e2afcb60f120940c38087582c8b
objlako <objlakodev@gmail.com>
2026-04-27 22:25:12 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace VoxelEngine;
+
+public class Program
+{
+    private static IWindow _window;
+    
+    public static void Main(string[] args)
+    {
+        
+        WindowOptions options = WindowOptions.Default with
+        {
+            Size = new Vector2D<int>(800, 600),
+            Title = "Voxel Engine!"
+        };
+        
+        _window = Window.Create(options);
+        
+        _window.Load += OnLoad;
+        _window.Update += OnUpdate;
+        _window.Render += OnRender;
+        
+        _window.Run();
+    }
+
+    private static void OnLoad()
+    {
+        IInputContext input = _window.CreateInput();
+        
+        for (int i = 0; i < input.Keyboards.Count; i++)
+            input.Keyboards[i].KeyDown += KeyDown;
+    }
+
+    private static void OnUpdate(double deltaTime)
+    {
+        Console.WriteLine("Updating...");
+    }
+
+    private static void OnRender(double deltaTime)
+    {
+        Console.WriteLine("Rendering...");
+    }
+
+    private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+    {
+        if (key == Key.Escape)
+            _window.Close();
+    }
+}
